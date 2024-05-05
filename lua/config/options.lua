6b6ee48a8e8d1548a05e8508145c7f8a88fa20a0
silverfoxy <babak@cloudflare.com>
2024-05-05 14:42:31 -0400

    Add fugitive, harpoon, and config neotree

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,7 @@
 -- Undercurl
 vim.cmd([[let &t_Cs = '\e[4:3m]']])
 vim.cmd([[let &t_Ce = '\e[4:0m]']])
+
+vim.g.autoformat = {
+  lua = false,
+}
--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -88,6 +88,7 @@ return {
         -- pyright will be automatically installed with mason and loaded with lspconfig
         pyright = {},
       },
+      autoformat = false,
     },
   },
 
@@ -166,34 +167,36 @@ return {
       })
     end,
   },
-
-  -- the opts function can also be used to change the default opts:
   {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    opts = function(_, opts)
-      table.insert(opts.sections.lualine_x, "😄")
-    end,
+    "letieu/harpoon-lualine",
+    dependencies = {
+      {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+      },
+    },
   },
 
   -- or you can return new options to override all the defaults
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
+    config = function()
+      require("lualine").setup({
+        lualine_c = { "harpoon2" },
+      })
+    end,
     opts = function()
       return {
         --[[add your custom lualine config here]]
       }
     end,
   },
-
   -- use mini.starter instead of alpha
   { import = "lazyvim.plugins.extras.ui.mini-starter" },
   -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.formatting.prettier" },
-  { import = "lazyvim.plugins.extras.coding.copilot" },
-
   -- add any tools you want to have installed below
   {
     "williamboman/mason.nvim",
